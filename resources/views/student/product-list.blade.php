5d31b12f05d7fa0dc736ea3eba96d38008b99477
rf-coder <bscs2312256@szabist.pk>
2024-12-23 23:53:48 +0500

    Update product-list.blade.php
    search bar

--- resources/views/student/product-list.blade.php
+++ resources/views/student/product-list.blade.php
@@ -1,12 +1,21 @@
-@foreach ($products as $product)
-<div class="col-md-4 mb-4">
-    <div class="card">
-        <img src="{{ asset('storage/' .$product->image) }}" class="card-img-top" alt="{{ $product->name }}">
-        <div class="card-body">
-            <h5 class="card-title">{{ $product->name }}</h5>
-            <p class="card-text">Rs. {{ $product->price }}/-</p>
-            <a href="{{ route('product-detail', $product->id) }}" class="btn btn-primary">View Product</a>
-        </div>
+<!-- resources/views/products/search.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Search Results for: "{{ $query }}"</h1>
+
+        @if($products->isEmpty())
+            <p>No products found.</p>
+        @else
+            <ul class="list-group">
+                @foreach($products as $product)
+                    <li class="list-group-item">
+                        <strong>{{ $product->name }}</strong> - {{ $product->category }}
+                    </li>
+                @endforeach
+            </ul>
+        @endif
     </div>
-</div>
-@endforeach
+@endsection
